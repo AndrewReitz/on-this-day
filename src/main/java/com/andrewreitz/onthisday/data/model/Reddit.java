780c957842971d315783259e05bdfc23bfcacb55
Andrew Reitz <aj.reitz@gmail.com>
2014-09-03 11:22:37 -0500

    Added Reddit Models

---
+++ src/main/java/com/andrewreitz/onthisday/data/model/Reddit.java
@@ -0,0 +1,15 @@
+package com.andrewreitz.onthisday.data.model;
+
+public final class Reddit {
+
+    private String kind;
+    private Data data;
+
+    public String getKind() {
+        return kind;
+    }
+
+    public Data getData() {
+        return data;
+    }
+}
